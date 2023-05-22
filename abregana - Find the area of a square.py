69d1b4d4d6f17e24bbbd99d9fef176f146532885
Macky0523 <71018332+Macky0523@users.noreply.github.com>
2023-05-22 12:37:24 +0800

    Update abregana - Find the area of a square.py

--- abregana - Find the area of a square.py
+++ abregana - Find the area of a square.py
@@ -3,3 +3,9 @@
 side_length = float(input("Enter the side length of the square: "))
 area = side_length ** 2
 print("The area of the square is:", area)
+
+if (area ==(side_legth **2)):
+  print("Test Result: Success")
+  
+  else:
+    print("Test Report: Failed")
