b3a9289e65be119c43288e8fe97b838bd06315a0
Macky0523 <71018332+Macky0523@users.noreply.github.com>
2023-06-18 20:04:21 +0800

    Update ML.py

--- ML.py
+++ ML.py
@@ -9,10 +9,9 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Load your dataset and split it into training and testing sets
 # X is the feature matrix, y is the target variable
-df = pd.read_csv('your_dataset.csv')
-X = dataset.drop('target', axis=1)
-y = dataset['target']
-
+df = pd.read_csv('heart.csv')
+X = df.drop('target', axis=1)
+y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Decision Tree
